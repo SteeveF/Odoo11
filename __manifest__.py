6c8fbb978a79cc0e972115a4fd31e3039cf58482
Steeve Favre <steeve@alcmedia.com>
2018-09-24 15:00:13 -0400

    Ok lets go with example...

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "C:\Users\steev\Desktop\migration",
+    'name': "toto",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
